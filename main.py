597f4e8758569f09e444a509bb2980f4c98b6a63
asvinod <asvinod@ucsc.edu>
2025-05-27 13:00:04 -0700

    Added code to main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+from titanic import clean_dataset, transform_dataset, predict
+
+def main():
+    test = pd.read_csv('input/test.csv')
+    train = pd.read_csv('input/train.csv')
+
+    test = clean_dataset(test)
+    train = clean_dataset(train)
+
+    transform_dataset(test, train)
+
+    predicted = predict(test, train)
+    predicted.to_csv("input/prediction.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
